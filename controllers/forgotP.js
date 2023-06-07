b7a112a827b8afaefb8662fc615dbbf68819228b
HawkWolf1 <singhavinash9fg@gmail.com>
2023-06-07 12:40:49 +0530

    forgotP trying

--- controllers/forgotP.js
+++ controllers/forgotP.js
@@ -19,38 +19,37 @@ require('dotenv').config()
 
 
 const forgotMyP = async (req, res) => {
-    try {
-        const { email } =  req.body;
-        const user = await myTable.findOne({where : { email }});
-        if(user){
-            const id = uuid.v4();
-            await forgotPassword.create({ id , 
-                active: true,
-                ourUserId: user.id })
+  try {
+    const { email } = req.body;
+    const user = await myTable.findOne({ email });
+    if (user) {
+      const id = uuid.v4();
+      await forgotPassword.create({
+        id,
+        active: true,
+        ourUserId: user._id,
+      });
 
-                .catch(err => {
-                    throw new Error(err)
-                })
-    
-            content = {
-                name: 'Avinash Singh', 
-                sender: {email: 'singhavinash9fg@gmail.com'},
-                to: [{email: email}],
-                subject: 'Forgot password',
-                textContent:'Reset your password here',
-                htmlContent: `<a href="http://localhost:4000/password/resetpassword/${id}">Reset password</a>`
-            }  
-           
-            await tranEmailApi.sendTransacEmail(content);   
-            return res.status(202).json({message: 'Link to reset password sent to your mail ', success: true, id:id})
-        } else {
-            return res.status(404).json({ message: 'User not found' });
-          }
-        } catch (error) {
-          console.log(error);
-          return res.status(500).json({ message: 'Internal server error' });
-        }
-      }
+      const content = {
+        name: 'Avinash Singh',
+        sender: { email: 'singhavinash9fg@gmail.com' },
+        to: [{ email: email }],
+        subject: 'Forgot password',
+        textContent: 'Reset your password here',
+        htmlContent: `<a href="http://localhost:4000/password/resetpassword/${id}">Reset password</a>`,
+      };
+
+      await tranEmailApi.sendTransacEmail(content);
+      return res.status(202).json({ message: 'Link to reset password sent to your mail', success: true, id: id });
+    } else {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 
     
 
