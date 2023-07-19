aebe659a49bc140fdb5c6a26fca6ad878d4eb6a5
HawkWolf1 <singhavinash9fg@gmail.com>
2023-07-19 13:28:11 +0530

    util changed a bit

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config() 
 
-const sequelize = new Sequelize('practise', 'root', process.env.GIT_PWD, {
+const sequelize = new Sequelize('practise', 'root', 'Bucketone23@', {
     dialect: 'mysql',
     host: 'localhost'
 })
